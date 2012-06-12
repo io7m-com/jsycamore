2e573fec4932553f368d90e4fb646fb6d548b6de
Mark Raynsford <code@io7m.com>
2012-06-12 15:53:53 +0000

    Add comment

--- src/com/io7m/jsycamore/Version.java
+++ src/com/io7m/jsycamore/Version.java
@@ -1,6 +1,14 @@
 package com.io7m.jsycamore;
 
+/**
+ * Version constants for the current implementation.
+ */
+
 public final class Version
 {
+  /**
+   * Version of theme files supported by this implementation.
+   */
+
   public final static int THEME_VERSION = 1;
 }
